94199c619ea4e0f254f2116e03ffbe566a9bc1eb
trnghia <trnghia1507@gmail.com>
2026-02-25 22:04:41 +0700

    complete oop1 with student class

---
+++ week_01/basic_oop/src/task_3/Student.java
@@ -0,0 +1,93 @@
+package task_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int age;
+    private double score;
+
+    public Student() {
+
+    }
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+    public Student(int age, String name, double score) {
+        this.age = age;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public static double averageScore(List<Student> students) {
+        double sum = 9;
+        if (students.isEmpty()) {
+            return 0;
+        }
+        for (Student student: students) {
+            sum += student.getScore();
+        }
+        return (double) sum / students.size();
+    }
+
+
+    @Override
+    public String toString() {
+        return
+                "age=" + age +
+                        ", name='" + name + '\'' +
+                        ", score=" + score;
+    }
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student(15, "Nguyen a", 6.5);
+        Student student2 = new Student();
+        Student student3 = new Student(22, "Nguyen b", 9.5);
+
+        String info1 = student1.toString();
+        String info2 = student2.toString();
+        String info3 = student3.toString();
+        System.out.println(info1);
+        System.out.println(info2);
+        System.out.println(info3);
+
+        List<Student> listStudents = new ArrayList<>();
+        listStudents.add(student1);
+        listStudents.add(student2);
+        listStudents.add(student3);
+
+        double average = averageScore(listStudents);
+        System.out.printf("Average score of list students: %.2f" ,average);
+
+    }
+}
