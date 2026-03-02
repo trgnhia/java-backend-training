9b573ed9e8022cecb40334f244bdaab50af2f157
trnghia1507 <trnghia1507@gmail.com>
2026-03-02 09:21:15 +0700

    push ex2 stream

--- week_02/StreamAPI/src/stream_practices/StreamEx2.java
+++ week_02/StreamAPI/src/stream_practices/StreamEx2.java
@@ -41,13 +41,22 @@ class User {
         this.salary = salary;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+
     public static void main(String[] args) {
         List<User> users = List.of(
                 new User("An", 22, 1200.0),
                 new User("Binh", 25, 1500.5),
-                new User("Cuong", 10, 2000.0),
+                new User("Cuong", 10, 20600.0),
                 new User("Dung", 28, 1800.75),
-                new User("Hanh", 5, 2500.0),
+                new User("Hanh", 5, 25050.0),
                 new User("Linh", 24, 1300.25)
         );
 
@@ -55,13 +64,37 @@ class User {
                 .filter(user -> user.getAge() > 18)
                 .map(user -> user.getName())
                 .collect(Collectors.toList());
+        System.out.println(over18);
 
         var summaryOfSalary = users.stream()
                 .map(User::getSalary)
                 .reduce(Double::sum);
         System.out.println(summaryOfSalary);
 
-        System.out.println(over18);
+        boolean isUnder18 = users.stream()
+                .anyMatch(user -> user.getAge() < 18);
+        System.out.println(isUnder18);
+
+        Optional<User> userHaveMaxSalary = users.stream()
+                .sorted(Comparator.comparingDouble(User::getSalary).reversed())
+                .findFirst();
+        System.out.println(userHaveMaxSalary);
+
+        List<User> filteredUser = users.stream()
+                .filter(user -> user.getAge() > 18)
+                .sorted(Comparator.comparingDouble(User::getSalary).reversed())
+                .limit(2)
+                .collect(Collectors.toList());
+        System.out.println(filteredUser);
+
+        List<String> usernameHaveThan6000Salary = users.stream()
+                .filter(user -> user.getSalary() > 6000)
+                .map(user -> user.getName())
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(usernameHaveThan6000Salary);
+
 
     }
 
@@ -96,6 +129,15 @@ public class StreamEx2 {
                 .reduce(0, Integer::sum);
         System.out.println(sumOfEvenNumbers);
 
+        Optional<Integer> firstGreaterThan10AfterSorted = numbers.stream()
+                .sorted()
+                .filter(n -> n > 10)
+                .findFirst();
+        System.out.println(firstGreaterThan10AfterSorted);
+
+        // reduce
+
+        List<String> words = Arrays.asList("java", "Springboot", "ReactJS", "NextJS");
 
     }
 }
