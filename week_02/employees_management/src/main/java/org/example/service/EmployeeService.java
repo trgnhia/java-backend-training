c6237c847bfd48453715c08e5f8b0321e8a0fc07
trnghia <trnghia1507@gmail.com>
2026-03-03 14:20:05 +0700

    add service of employee

--- week_02/employees_management/src/main/java/org/example/service/EmployeeService.java
+++ week_02/employees_management/src/main/java/org/example/service/EmployeeService.java
@@ -1,4 +1,12 @@
 package org.example.service;
 
+import org.example.model.Employee;
+
+import java.util.Optional;
+
 public interface EmployeeService {
+    void addEmployee(Employee employee);
+    void removeEmployee (String id);
+    void updateEmployee (String id);
+    Optional<Employee> findById (String id);
 }
--- week_02/employees_management/src/main/java/org/example/service/EmployeeServiceImpl.java
+++ week_02/employees_management/src/main/java/org/example/service/EmployeeServiceImpl.java
@@ -1,4 +1,30 @@
 package org.example.service;
 
-public class EmployeeServiceImpl {
+import org.example.model.Employee;
+
+import java.util.Map;
+import java.util.*;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    private final Map<String, Employee> employyes = new HashMap<>();
+
+    @Override
+    public void addEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public void removeEmployee(String id) {
+
+    }
+
+    @Override
+    public void updateEmployee(String id) {
+
+    }
+
+    @Override
+    public Optional<Employee> findById(String id) {
+        return Optional.empty();
+    }
 }
