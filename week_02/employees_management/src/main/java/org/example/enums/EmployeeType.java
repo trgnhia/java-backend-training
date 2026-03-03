65408795cf743bade7ac2e8135d03378f778616a
trnghia <trnghia1507@gmail.com>
2026-03-03 13:58:37 +0700

    create model employee

---
+++ week_02/employees_management/src/main/java/org/example/enums/EmployeeType.java
@@ -0,0 +1,7 @@
+package org.example.enums;
+
+public enum EmployeeType {
+    FULL_TIME,
+    INTERN,
+    ONSITE;
+}
--- week_02/employees_management/src/main/java/org/example/model/Employee.java
+++ week_02/employees_management/src/main/java/org/example/model/Employee.java
@@ -1,5 +1,49 @@
 package org.example.model;
 
+import org.example.enums.EmployeeType;
+import org.example.enums.Status;
+
+import java.time.LocalDate;
+
 public class Employee {
+    private String id;
     private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private double salary;
+    private LocalDate hiredDate;
+    private EmployeeType employeeType;
+    private Status  status;
+
+    public Employee(String department, String email, EmployeeType employeeType, LocalDate hiredDate, String id, String name, String phone, double salary, Status status) {
+        this.department = department;
+        this.email = email;
+        this.employeeType = employeeType;
+        this.hiredDate = hiredDate;
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.salary = salary;
+        this.status = status;
+    }
+
+    public Employee() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "department='" + department + '\'' +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", hiredDate=" + hiredDate +
+                ", employeeType=" + employeeType +
+                ", status=" + status +
+                '}';
+    }
 }
